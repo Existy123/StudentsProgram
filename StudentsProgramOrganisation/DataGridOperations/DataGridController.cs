b7f56bf6cf158ff48417745ae77fa87cadf1c3e1
cebilon123 <michalkowal567@gmail.com>
2019-05-16 22:23:54 +0200

    First Commit
    First commit

---
+++ StudentsProgramOrganisation/DataGridOperations/DataGridController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace StudentsProgramOrganisation.DataGridOperations
+{
+    using DataBase;
+    public class DataGridController
+    {
+        public DataGrid DataGrid { get; set; }
+
+        public DataGridController(DataGrid dataGrid)
+        {
+            this.DataGrid = dataGrid;
+        }
+
+        public void SetDataSource()
+        {
+            try
+            {
+                StudentsOrgEntities entities = new StudentsOrgEntities();
+
+                ICollection<DataBase.Subjects> subjects = new List<Subjects>();
+
+                foreach (var subject in entities.Subjects)
+                {
+                    subjects.Add(subject);
+                }
+
+                this.DataGrid.ItemsSource = OrderSubjectsByDate(subjects);
+               
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Nie można nawiązać połączenia.");
+                App.Current.Shutdown();
+            }           
+        }
+
+        private ICollection<Subjects> OrderSubjectsByDate (ICollection<Subjects> subjects)
+        {
+            ICollection<Subjects> orderedSubjects = subjects.Select(n => n).OrderBy(n => n.subjectTime).ToList();
+
+            return orderedSubjects;
+        }
+    }
+}
---
+++ StudentsProgramOrganisation/DataGridOperations/SubjectController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentsProgramOrganisation.DataBase;
+namespace StudentsProgramOrganisation.DataGridOperations
+{
+    public class SubjectController
+    {
+        private readonly StudentsOrgEntities _studentsOrgEntities = new StudentsOrgEntities();
+
+        public void AddSubject(Subjects subject)
+        {
+            var subjectsDb = _studentsOrgEntities.Subjects;
+
+            if (SubjectValidation(subject)) subjectsDb.Add(subject);
+
+        }
+        public void DeleteSubject(int id)
+        {
+
+        }
+
+        // Unit testing = false;
+        // gitHuB
+        public bool SubjectValidation(Subjects subject)
+        {
+            if (subject.subjectName != null && string.IsNullOrEmpty(subject.subjectName) != true)
+            {
+                if (subject.subjectTime != null) return true;
+
+                else return false;
+            }               
+
+                else return false;
+        }
+    }
+}
